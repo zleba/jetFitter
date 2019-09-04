16d2317dcab513b79e9c053b9255a5a49165b76f
Radek Zlebcik <zlebcr@mail.desy.de>
2019-09-04 15:30:31 +0200

    Improving xFitter running

--- farm/scenarios/runAll.sh
+++ farm/scenarios/runAll.sh
@@ -1,2 +1,4 @@
+export LD_LIBRARY_PATH_STORED=$LD_LIBRARY_PATH
+
 f=yScan
 condor_submit  -batch-name `basename $f` dirName=$f nFiles=4  xfitter.submit
--- farm/scenarios/xfitter.sh
+++ farm/scenarios/xfitter.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-cd /nfs/dust/cms/user/zlebcr/xFitting
-source setup.sh
+#cd /nfs/dust/cms/user/zlebcr/xFitting
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_STORED
 cd /nfs/dust/cms/user/zlebcr/xFitting/farm/scenarios/yScan/variants/y$1
-ln -s /nfs/dust/cms/user/zlebcr/xFitting/xfitter-2.0.1/datafiles
+ln -s /nfs/dust/cms/user/zlebcr/xFitting/farm/datafiles
 xfitter
