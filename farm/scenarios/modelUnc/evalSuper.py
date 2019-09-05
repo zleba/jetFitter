3b8c5cf5c90fc1cf1a27ae9e1ae9f3f07a8c6a38
Radek Zlebcik <zlebcr@mail.desy.de>
2019-09-05 11:22:27 +0200

    Model unc

--- farm/scenarios/modelUnc/evalSuper.py
+++ farm/scenarios/modelUnc/evalSuper.py
@@ -1,46 +1,28 @@
 #!/usr/bin/env python
 
 
-from shutil import copyfile
 import os
 if not os.path.exists('variants'):
     os.mkdir('variants')
 
-scales = ((1,1), (2,1), (0.5,1), (1,2), (1,0.5), (2,2), (0.5,0.5) )
-
-def valErr(cnt, err1, err2 = -999):
-    if err2 == -999:
-        return [cnt, cnt+err1, cnt-err1]
-    else:
-        return [cnt, cnt+err1, cnt+err2]
-
-
+pars = {}
+pars['Q02']      =  [1.9, 2.2, 1.6]
 pars['mCh']  =  [1.47, 1.53, 1.41]
 pars['mBt'] =  [4.5, 4.75, 4.25]
 pars['fS'] = [ 0.4, 0.5, 0.3]
-pars['mu']      =  [(1,1), (2,1), (0.5,1), (1,2), (1,0.5), (2,2), (0.5,0.5) ] #7 point variation
-pars['Q0']      =  [1.9, 2.2, 1.6]
 pars['q2MinData'] = [3.5, 5.0, 2.5]
+pars['Dg']  =  ['*  ', '   ']
+pars['Ddv'] =  ['*   ', '    ']
+pars['mu']      =  [(1,1), (2,1), (0.5,1), (1,2), (1,0.5), (2,2), (0.5,0.5) ] #7 point variation
         
-variations = ("mu", "mCh", "mBt", "fS", "q2MinData")
-
-def nVars():
-    nv = 0;
-    for v in variations:
-        nv += len(pars[v]) - 1
-    return nv+1
-
-
-def getSh(v):
-    iv = 0
-    for v in variations:
-        nv += len(pars[v]) - 1
+variations = ('Q02', 'mCh', 'mBt', 'fS', 'q2MinData', 'Dg', 'Ddv', 'mu')
 
 v = 0
 for vName in variations:
-    for 
-    for fTag in ("steering", "minuit.in", "ewparam"):
-        for v in range(nVars()):
+    s = 0 if v == 0 else 1
+    for val in pars[vName][s:]:
+
+        for fTag in ("steering", "minuit.in", "ewparam"):
             dirName = 'variants/v'+str(v)
             if not os.path.exists(dirName):
                 os.mkdir(dirName)
@@ -53,59 +35,22 @@ for vName in variations:
             with open(fName, "wt") as fout:
 
                 for line in fin:
+                    for vNow in variations:
+                        if vNow == 'mu': #for scale variations
+                            if vNow == vName:
+                                line = line.replace('@'+vNow+'R', str(val[0]))
+                                line = line.replace('@'+vNow+'F', str(val[1]))
+                            else:
+                                line = line.replace('@'+vNow+'R', str(pars[vNow][0]))
+                                line = line.replace('@'+vNow+'F', str(pars[vNow][1]))
+                            continue
 
-
-                    for 
-
-                    #scale variations unc., 7 points
-                    vs = v
-                    if v >= 7:
-                        vs = 0
-                    
-                    line = line.replace('$muR', str(scales[vs][0]))
-                    line = line.replace('$muF', str(scales[vs][1]))
-
-                    #Starting scale of evolution
-
-
-
-
-                    #DIS Q2 cut unc
-                    if v == 7:
-                        line = line.replace('$q2MinData', str(5.0))
-                    elif v == 8:
-                        line = line.replace('$q2MinData', str(2.5))
-                    else:
-                        line = line.replace('$q2MinData', str(3.5))
-
-
-                    #charm mass
-                    if v == 9:
-                        line = line.replace('$mCh', str(1.53))
-                    elif v == 10:
-                        line = line.replace('$mCh', str(1.41))
-                    else:
-                        line = line.replace('$mCh', str(1.47))
-
-                    #bottom mass
-                    if v == 11:
-                        line = line.replace('$mBt', str(4.75))
-                    elif v == 12:
-                        line = line.replace('$mBt', str(4.25))
-                    else:
-                        line = line.replace('$mBt', str(4.5))
-
-                    #fS variation
-                    if v == 13:
-                        line = line.replace('$fS', str(0.5))
-                    elif v == 14:
-                        line = line.replace('$fS', str(0.3))
-                    else:
-                        line = line.replace('$fS', str(0.4))
-
-
+                        if vNow == vName:
+                            line = line.replace('@'+vNow, str(val))
+                        else: #replace by central value
+                            line = line.replace('@'+vNow, str(pars[vNow][0])) 
 
 
                     fout.write(line)
-
+        v += 1
 
