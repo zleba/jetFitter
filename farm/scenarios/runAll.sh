cb314082dfc2f2bd0400375e39ea8c80bde858fd
Radek Zlebcik <zlebcr@mail.desy.de>
2019-09-09 15:22:45 +0200

    Adding ak7 files

--- farm/scenarios/runAll.sh
+++ farm/scenarios/runAll.sh
@@ -1,9 +1,13 @@
 export LD_LIBRARY_PATH_STORED=$LD_LIBRARY_PATH
 
 #f=yScanAs
-f=modelUncEng
-nF=`ls -d -1 $f/variants/v*/   | wc -l`
-echo $nF
-mkdir -p $f/logs
-#exit
-condor_submit  -batch-name `basename $f` dirName=$f nFiles=$nF  xfitter.submit
+#f=modelUncHERA
+for f in modelUncHighQ  modelUncHighQHERA
+#for f in modelUncAsHighQ
+do
+    nF=`ls -d -1 $f/variants/v*/   | wc -l`
+    echo $nF
+    mkdir -p $f/logs
+    #exit
+    condor_submit  -batch-name `basename $f` dirName=$f nFiles=$nF  xfitter.submit
+done
