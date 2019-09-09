97bfed185c31732edaca8a12d257f04858bf3568
Radek Zlebcik <zlebcr@mail.desy.de>
2019-09-09 09:19:42 +0200

    Adding data of Engin

--- farm/scenarios/runAll.sh
+++ farm/scenarios/runAll.sh
@@ -4,5 +4,6 @@ export LD_LIBRARY_PATH_STORED=$LD_LIBRARY_PATH
 f=modelUncEng
 nF=`ls -d -1 $f/variants/v*/   | wc -l`
 echo $nF
+mkdir -p $f/logs
 #exit
 condor_submit  -batch-name `basename $f` dirName=$f nFiles=$nF  xfitter.submit
